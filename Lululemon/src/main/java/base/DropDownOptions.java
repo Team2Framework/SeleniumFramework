4fd9665f400c523a68dea032db688a4f47c74e2d
DOLA DAOUD <45337466+DOLADAOUD@users.noreply.github.com>
2019-01-15 16:15:26 -0500

    Add files via upload

--- Lululemon/src/main/java/base/DropDownOptions.java
+++ Lululemon/src/main/java/base/DropDownOptions.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class DropDownOptions extends CommonAPI {
     public void dropDown(){
-        List<WebElement> element = getListOfWebElementsByCss(".//ul[@class='primary']//li[1] option");
+        List<WebElement> element = getListOfWebElementsByCss(".//ul[@id='js-navigation-menu']//li[1] option");
         List<String> listOfText = getListOfString(element);
         for (String text : listOfText) {
             System.out.println(text);
